1f215b5a5d15c4efb4948a157b4d2464cf611140
Dominik Skórka <Sk0rka.dev@gmail.com>
2024-02-24 15:02:22 +0100

    NavBar

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ https://clerk.com/docs/references/nextjs/custom-signup-signin-pages
 //---------------------------------------------//
 npx shadcn-ui@latest init komponenty shadcn
 https://ui.shadcn.com/docs/components/accordion
+//---------------------------------------------//
+https://lucide.dev/icons/ icons
--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -1,10 +1,48 @@
 "use client";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, useAuth } from "@clerk/nextjs";
+import Container from "../Container";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Button } from "../ui/button";
+import SearchInput from "../SearchInput";
 
 const NavBar = () => {
+  const router = useRouter();
+  const { userId } = useAuth();
   return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
+    <div className="sticky top-0 border border-b-primary/10 bg-secondary">
+      <Container>
+        <div className="flex justify-between items-center">
+          <div
+            className="flex items-center gap-1 cursor-pointer"
+            onClick={() => router.push("/")}
+          >
+            <Image src="/AsnetBg.png" alt="logo" width="60" height="60" />
+            <div className="font-bold text-xl">Happy Vacations</div>
+          </div>
+
+          <SearchInput />
+
+          <div className="flex gap-3 items-center">
+            <div>theme</div>
+            <UserButton afterSignOutUrl="/" />
+            {!userId && (
+              <>
+                <Button
+                  onClick={() => router.push("sign-in")}
+                  variant="outline"
+                  size="sm"
+                >
+                  Sign in
+                </Button>
+                <Button onClick={() => router.push("sign-up")} size="sm">
+                  Sign up
+                </Button>
+              </>
+            )}
+          </div>
+        </div>
+      </Container>
     </div>
   );
 };
