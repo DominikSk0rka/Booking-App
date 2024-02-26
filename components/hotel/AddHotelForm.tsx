3e21747aa62c4e4950e1d68c61da2c4b8c525367
Dominik Skórka <Sk0rka.dev@gmail.com>
2024-02-26 13:10:08 +0100

    Form schema in Zod

--- components/hotel/AddHotelForm.tsx
+++ components/hotel/AddHotelForm.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Hotel, Room } from "@prisma/client";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
 
 interface AddHotelProps {
   hotel: HotelWithRooms | null;
@@ -8,7 +11,67 @@ interface AddHotelProps {
 export type HotelWithRooms = Hotel & {
   rooms: Room[];
 };
+
+const formSchema = z.object({
+  title: z.string().min(3, {
+    message: "Title must be atleast 3 characters long",
+  }),
+  description: z.string().min(10, {
+    message: "Description must be atleast 10 characters long",
+  }),
+  image: z.string().min(1, {
+    message: "Image is required",
+  }),
+  country: z.string().min(1, {
+    message: "Country is required",
+  }),
+  city: z.string().optional(),
+  state: z.string().optional(),
+  locationDescription: z.string().min(10, {
+    message: "Description must be atleast 10 characters long",
+  }),
+
+  gym: z.boolean().optional(),
+  spa: z.boolean().optional(),
+  laundry: z.boolean().optional(),
+  restourant: z.boolean().optional(),
+  freeParking: z.boolean().optional(),
+  bikeRental: z.boolean().optional(),
+  freeWifi: z.boolean().optional(),
+  movieNights: z.boolean().optional(),
+  swimmingPool: z.boolean().optional(),
+  coffeShop: z.boolean().optional(),
+});
+
 const AddHotelForm = ({ hotel }: AddHotelProps) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      image: "",
+      country: "",
+      city: "",
+      state: "",
+      locationDescription: "",
+
+      gym: false,
+      spa: false,
+      laundry: false,
+      restourant: false,
+      freeParking: false,
+      bikeRental: false,
+      freeWifi: false,
+      movieNights: false,
+      swimmingPool: false,
+      coffeShop: false,
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return <div>Add</div>;
 };
 
